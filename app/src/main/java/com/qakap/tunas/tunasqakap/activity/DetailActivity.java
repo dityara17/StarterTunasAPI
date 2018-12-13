99afa0f6156b454770e9e7d2e9fe4ebaa91dd016
Qampus <hasan.shobri@outlook.com>
2018-12-13 22:19:21 +0700

    Initial commit

---
+++ app/src/main/java/com/qakap/tunas/tunasqakap/activity/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.qakap.tunas.tunasqakap.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qakap.tunas.tunasqakap.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
